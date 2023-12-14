748c42f3164b482f8be3d69d1df8cdb70294eb37
viswa <saravanan.resume@gmail.com>
2023-12-14 21:25:31 +0800

    debug

--- components/codility/count_triangles.js
+++ components/codility/count_triangles.js
@@ -7,7 +7,7 @@ export const count_triangles = (A = [10, 2, 5, 1, 8, 20]) => {
   console.log(A)
   for (let i = 0; i < A.length - 2; i++) {
     if (A[i] >= 0 && A[i] + A[i + 1] > A[i + 2]) {
-      console.log(A[i], A[i + 1], A[i + 2])
+      console.log(` ${A[i]} + ${A[i + 1]} > ${A[i + 2]} `)
       count++
     }
   }
